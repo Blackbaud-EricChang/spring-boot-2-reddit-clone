2f8e6837397f2db773e40336239c17726a66c6c3
Eric Chang <eric.chang@blackbaud.me>
2019-04-26 08:04:49 -0500

    [Domain]: Create entity stubs using Lumbok

---
+++ src/main/java/com/efro/springit/domain/Comment.java
@@ -0,0 +1,24 @@
+package com.efro.springit.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Comment {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private Long linkId;
+
+    private String body;
+
+    // Link
+}
