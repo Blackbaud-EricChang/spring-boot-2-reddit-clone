ed6e6f28a5dad5e00be6b8630092581de888d3e1
Eric Chang <eric.chang@blackbaud.me>
2019-04-29 08:19:54 -0500

    [Domain]: Create Auditable Super Class to handle @CreatedDate, @CreatedBy, @LastModifiedBy, and @LastModifiedDate for the Entities.

---
+++ src/main/java/com/efro/springit/domain/Auditable.java
@@ -0,0 +1,64 @@
+package com.efro.springit.domain;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+/**
+ * This is an important super class to define the Auditing info for all @Data Entities, such as CreatedDate and CreatedBy.
+ * The Entities will just need to extend this class.
+ */
+@MappedSuperclass                                   // [KEY]
+@EntityListeners(AuditingEntityListener.class)      // [KEY]
+public abstract class Auditable {
+
+    @CreatedBy
+    private String createdBy;
+
+    @CreatedDate
+    private LocalDateTime creationDate;
+
+    @LastModifiedBy
+    private String lastModifiedBy;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public String getLastModifiedBy() {
+        return lastModifiedBy;
+    }
+
+    public void setLastModifiedBy(String lastModifiedBy) {
+        this.lastModifiedBy = lastModifiedBy;
+    }
+
+    public LocalDateTime getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(LocalDateTime lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+}
